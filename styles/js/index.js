cc1e89b92663ff23c29dc9995566cc65529b0e7b
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-08-13 21:15:34 -0700

    specific content navigator script added

--- styles/js/index.js
+++ styles/js/index.js
@@ -21,4 +21,30 @@ function navigateToHomepage() {
                 modal.style.display = "none";
             }
         });
+
+        //navigate and highlight contents in the main section
+        const sidebarLinks = document.querySelectorAll('.table-contents a');
+        const mainContentSections = document.querySelectorAll('.item2 > *'); // Select main content sections
+        
+        sidebarLinks.forEach(link => {
+           link.addEventListener('click', function(event) {
+              event.preventDefault();
+        
+              const targetSectionId = this.getAttribute('data-target'); // Get the data-target attribute (e.g., "#section1")
+              const targetSection = document.querySelector(targetSectionId);
+        
+              mainContentSections.forEach(section => {
+                section.classList.remove('highlighted');
+              });
+        
+              // Add highlight class to the corresponding section
+              if (targetSection) {
+                 targetSection.classList.add('highlighted');
+                 // Scroll to the target section instantly
+                 targetSection.scrollIntoView({ behavior: 'auto' });
+              }
+           });
+        });
+        
+
   
